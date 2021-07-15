527d67785a61c66268a90cd68f3c82405e8122f9
Douglas A B Novato <douglasabnovato@gmail.com>
2021-07-15 10:57:03 -0300

    116/399 componente card
    estrutura, estilização, componentização

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ function App() {
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Card/>  
-        <Card/>  
-        <Card/>  
-        <Card/>  
+        <Card title="Card 1" red>
+          X
+        </Card>  
+        <Card title="Card 2" green>
+          Y
+        </Card>  
       </div> 
       <div className="linha">
-        <Card/>  
-        <Card/>  
-        <Card/>  
-        <Card/>  
+      <Card title="Card 1" blue>
+          X
+        </Card> 
+        <Card title="Card 2" purple>
+          Y
+        </Card>     
       </div> 
     </div>
   );
