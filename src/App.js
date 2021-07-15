c91e13fba7a4d151266b48dbd4d0c1805bada3c1
Douglas A B Novato <douglasabnovato@gmail.com>
2021-07-15 15:48:58 -0300

    120/399 sem redux
    implementar as funcionalidades dos quatro componentes sem redux com comunicação direta e indireta

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
+
 import Intervalo from "./components/Intervalo";
 import Media from "./components/Media";
 import Soma from "./components/Soma";
 import Sorteio from "./components/Sorteio";
-import "./App.css" 
 
+import "./App.css" 
+ 
 function App() {
+
+  const [min, setMin] = useState(550)
+  const [max, setMax] = useState(1020)
+
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Intervalo/> 
+        <Intervalo min={min} max={max} onMinChanged={setMin} onMaxChanged={setMax}/> 
       </div> 
       <div className="linha"> 
-        <Media/>
-        <Soma/>
-        <Sorteio/>   
+        <Media min={min} max={max}/>
+        <Soma min={min} max={max}/>
+        <Sorteio min={min} max={max}/>   
       </div> 
     </div>
   );
--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -3,12 +3,15 @@ import React from "react"
 import Card from "./Card"
 
 export default props => {
+    
+    const { min, max } = props 
+
     return(
         <Card title="Sorteio de um Número" purple> 
             <div className="Intervalo">
                 <span>
                     <span>Resultado: </span>
-                    <strong>7</strong>
+                    <strong>{ (Math.random() * (max - min) + min).toFixed(2)}</strong>
                 </span> 
             </div>
         </Card>
